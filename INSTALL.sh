4a2845621345689d582079953ec04803b97e04cc
cycoe <cycoe@cycoe.win>
2017-04-04 08:45:38 +0800

    modify the install.sh

---
+++ INSTALL.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo mkdir /usr/share/pyshared
+sudo cp -r lovewallpaper /usr/share/pyshared/
+sudo cp data/love-wallpaper.png /usr/share/icons
+sudo cp data/love-wallpaper.desktop /usr/share/applications
+sudo cp love-wallpaper /usr/bin/
---
+++ lovewallpaper/lib/VERSION.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+__VERSION__ = "1.5.4"
+__VERSION_CODE__ = 9
